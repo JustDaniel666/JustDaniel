2c83c7cebd2b289be2cb52809376354e0749c009
JustDaniel666 <63105126+JustDaniel666@users.noreply.github.com>
2020-04-03 14:06:55 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+let name = "Daniel";
+let age = 14;
+let school = "crompton house";
+
+console.log(" hi my name is " + name);
+
+console.log(" i am " + age + " years old ");
+
+console.log("i attend " + school );
+let numberOne = 10;
+let numberTwo = 20;
+let total = numberOne + numberTwo;
+const addition = (numberOne, numberTwo) => {
+    total = numberOne + numberTwo
+    console.log(total);
+}; 
+
+addition(20, 30);
+addition(30, 70);
+addition(40, 15);
+addition(400, 150)
